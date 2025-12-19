9a304bd293e41a7b4c21c6963e56cccc5d191d95
Ganto23 <godfrey.antomarlin@gmail.com>
2025-12-19 19:23:07 +0000

    Added optional.hpp template

---
+++ include/nstl/optional.hpp
@@ -0,0 +1,19 @@
+#include <cstddef>
+#include <stdexcept>
+#include <memory>
+#include <iostream>
+#include <utility>
+#include <cstring>
+#include <type_traits>
+#include <algorithm>
+#include <compare>
+
+namespace nstl {
+    template<typename T>
+    class optional {
+    public:
+
+    private:
+
+    };
+}
